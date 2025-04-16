6903b3e42eeef648c36de442f77255661c9eacf6
张鑫 <22373090@buaa.edu.cn>
2025-04-16 09:17:06 +0800

    [fix] 修改import顺序不当导致相对目录无法使用的Bug

--- NMB_Map/src/algorithm/main.py
+++ NMB_Map/src/algorithm/main.py
@@ -1,15 +1,15 @@
 import os
 
-from commandHandler import CommandHandler
-
 from logger_config import setup_logger
 
 # 初始化日志记录器
 logger = setup_logger()
 # 设置当前工作目录
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
 os.chdir(current_dir)
+
+from commandHandler import CommandHandler
+
 if __name__ == "__main__":
     logger.info("the program is starting")
     command_handler = CommandHandler()
